0633e4d500ed86b60693db4034b008c3203b40d1
Michael <jfkdsjfks@bths.edu>
2026-05-05 10:54:20 -0400

    Created the Assignments Creator Class

---
+++ src/main/java/com/project_saturn/Project_Tables/AssignmentsCreator.java
@@ -0,0 +1,30 @@
+package com.project_saturn.Project_Tables;
+
+import com.project_saturn.Project_Objects.Assignments;
+
+import java.util.ArrayList;
+
+public class AssignmentsCreator {
+
+    private ArrayList<Assignments> assignments;
+
+    public AssignmentsCreator() {
+        assignments = new ArrayList<>();
+    }
+
+    public void makeAssignments() {
+        for (int i = 0; i < CourseTable.getCourses().size(); i++) {
+            for (int j = 1; j <= 15; j++) {
+                if (j <= 12) {
+                    assignments.add(new Assignments(1, i, "Minor", j, (int) (Math.random() * 26) + 75));
+                } else {
+                    assignments.add(new Assignments(1, i, "Major", j, (int) (Math.random() * 26) + 75));
+                }
+            }
+        }
+    }
+
+    public ArrayList<Assignments> getAssignments() {
+        return assignments;
+    }
+}
