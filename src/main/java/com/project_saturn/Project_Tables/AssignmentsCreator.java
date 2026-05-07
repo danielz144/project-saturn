00d2782073d01076713f52253f8aeb625dd9bfeb
Michael <jfkdsjfks@bths.edu>
2026-05-07 10:16:07 -0400

    Added some comments

--- src/main/java/com/project_saturn/Project_Tables/AssignmentsCreator.java
+++ src/main/java/com/project_saturn/Project_Tables/AssignmentsCreator.java
@@ -1,7 +1,6 @@
 package com.project_saturn.Project_Tables;
 
 import com.project_saturn.Project_Objects.Assignments;
-
 import java.util.ArrayList;
 
 public class AssignmentsCreator {
@@ -16,15 +15,15 @@ public class AssignmentsCreator {
         for (int i = 0; i < CoursesTable.getCourses().size(); i++) {
             for (int j = 1; j <= 15; j++) {
                 if (j <= 12) {
-                    assignments.add(new Assignments(1, i, "Minor", j, (int) (Math.random() * 26) + 75));
+                    assignments.add(new Assignments(1, i, "Minor", j, (int) (Math.random() * 26) + 75)); // Replace student id with one from CourseTable
                 } else {
-                    assignments.add(new Assignments(1, i, "Major", j, (int) (Math.random() * 26) + 75));
+                    assignments.add(new Assignments(1, i, "Major", j, (int) (Math.random() * 26) + 75)); // Replace student id with one from CourseTable
                 }
             }
         }
     }
 
     public ArrayList<Assignments> getAssignments() {
-        return assignments;
+        return assignments; // Returns the list of Assignments
     }
 }
